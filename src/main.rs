78efc9dcfb5ea0b161bd3b4fada5f7269f4d370e
Karolin Varner <karo@cupdev.net>
2024-02-06 22:59:38 +0100

    feat: Inline Index::from_file into create()

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::io::{SeekFrom, Seek};
 
 use anyhow::{ensure, Result};
 use clap::{Args, Parser, Subcommand};
@@ -46,18 +47,58 @@ struct CommandInvocation<Command> {
 }
 
 fn create(args: &CommandInvocation<CreateCommand>) -> Result<Index> {
-    let CreateCommand { ref name, ref path } = args.command;
+    use crate::index::*;
 
     ensure!(
         args.index.is_none(),
         "Refusing to overwrite existing index!"
     );
-    let mut idx = Index::from_file(path)?;
-    if let Some(n) = name.as_ref() {
-        idx.meta.name.push(n.to_owned())
-    }
 
-    Ok(idx)
+    let CreateCommand { ref name, ref path } = args.command;
+
+    let file_len = fs::File::open(path)?.seek(SeekFrom::End(0))?;
+
+    let dbg_canonical = match fs::canonicalize(path) {
+        Ok(v) => format!("{:?}", v)
+            .trim_start_matches('"')
+            .trim_end_matches('"')
+            .to_owned(),
+        Err(e) => format!("<Error: {:?}>", e),
+    };
+
+    let main_frag = Fragment {
+        meta: Meta {
+            name: vec!["main".to_owned()],
+            comment: vec![
+                format!("Relative path during fragment creation: {path}"),
+                format!("Canonical path during fragment creation: {dbg_canonical}"),
+            ],
+        },
+        location: File {
+            device: None,
+            path: path.to_owned(),
+        }
+            .as_location(),
+        hashes: Default::default(),
+        geometry: Slice {
+            start: 0,
+            end: file_len,
+        },
+        holes: vec![],
+    };
+
+    let index = Index {
+        meta: Meta {
+            name: name.iter().by_ref().map(|v| v.to_owned()).collect(),
+            comment: vec![
+                format!("Relative path during creation: {path}"),
+                format!("Canonical path during creation: {dbg_canonical}"),
+            ],
+        },
+        fragments: vec![main_frag],
+    };
+
+    Ok(index)
 }
 
 fn write_fragment(_args: &CommandInvocation<WriteFragmentCommand>) -> Result<Index> {
